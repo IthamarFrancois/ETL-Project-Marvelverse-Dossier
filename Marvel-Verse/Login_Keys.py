bd8ffc4e75f8191857e40d789362164db7a372ba
Ithamar Francois <77698337+IthamarFrancois@users.noreply.github.com>
2021-04-29 23:58:50 -0400

    Submitting completed ETL Project.

---
+++ Marvel-Verse/Login_Keys.py
@@ -0,0 +1,15 @@
+# Marvel API Key
+
+ts_Hash_API = "&ts=1&hash=c7c3799af5d55f5afda1180ecb5fb550"
+Marvel_API = "&apikey=e157759c23987b9631b87c9940a5eebd&ts=1&hash=c7c3799af5d55f5afda1180ecb5fb550"
+
+## https://gateway.marvel.com/v1/public/characters?name= GIANT%20MAN &apikey=e157759c23987b9631b87c9940a5eebd&ts=1&hash=c7c3799af5d55f5afda1180ecb5fb550
+## "A md5 digest of the ts parameter, your private key and your public key (e.g. md5  (ts+privateKey+publicKey), hash in lowercase" 
+## See API documentation at: https://developer.marvel.com/documentation/authorization
+
+
+# MongoClient
+
+CompassID = "Test"
+CompassPsswrd = "Test1234"
+CompassString = f'mongodb+srv://{CompassID}:{CompassPsswrd}@ithamarfrancois.y3l8v.mongodb.net/test'
